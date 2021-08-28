1ced95a9cf52efa4c6fb060b5aae8bce345af831
IgorAvshtol <iharrwe@gmail.com>
2021-08-28 13:39:45 +0300

    Used map in TaskFormContainer.tsx

--- src/component/TaskFormContainer.tsx
+++ src/component/TaskFormContainer.tsx
@@ -2,35 +2,23 @@ import { useState } from "react";
 import { Box } from "./Box";
 import classes from "./Box.module.css"
 
+
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
 export function TaskFormContainer() {
-    const [index, setIndex] = useState(3);
+
+    const [index, setIndex] = useState(value[2]);
 
     function moveCard(i: number) {
         setIndex(i)
     }
 
+
     return (
-        <div className={classes.containerStyle} >
-            <Box card={index === 1} moveCard={moveCard.bind(null, 1)}></Box>
-            <Box card={index === 2} moveCard={moveCard.bind(null, 2)}></Box>
-            <Box card={index === 3} moveCard={moveCard.bind(null, 3)}></Box>
-            <Box card={index === 4} moveCard={moveCard.bind(null, 4)}></Box>
-            <Box card={index === 5} moveCard={moveCard.bind(null, 5)}></Box>
-            <Box card={index === 6} moveCard={moveCard.bind(null, 6)}></Box>
-            <Box card={index === 7} moveCard={moveCard.bind(null, 7)}></Box>
-            <Box card={index === 8} moveCard={moveCard.bind(null, 8)}></Box>
-            <Box card={index === 9} moveCard={moveCard.bind(null, 9)}></Box>
-            <Box card={index === 10} moveCard={moveCard.bind(null, 10)}></Box>
-            <Box card={index === 11} moveCard={moveCard.bind(null, 11)}></Box>
-            <Box card={index === 12} moveCard={moveCard.bind(null, 12)}></Box>
-            <Box card={index === 13} moveCard={moveCard.bind(null, 13)}></Box>
-            <Box card={index === 14} moveCard={moveCard.bind(null, 14)}></Box>
-            <Box card={index === 15} moveCard={moveCard.bind(null, 15)}></Box>
-            <Box card={index === 16} moveCard={moveCard.bind(null, 16)}></Box>
-            <Box card={index === 17} moveCard={moveCard.bind(null, 17)}></Box>
-            <Box card={index === 18} moveCard={moveCard.bind(null, 18)}></Box>
-            <Box card={index === 19} moveCard={moveCard.bind(null, 19)}></Box>
-            <Box card={index === 20} moveCard={moveCard.bind(null, 20)}></Box>
+        <div className={classes.containerStyle}>
+            {value.map(v => {
+                return <Box card={index === v} moveCard={moveCard.bind(null, v)}/>
+            })}
         </div>
     );
 }
